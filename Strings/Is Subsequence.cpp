bc6d7986f3fc0e2ce3407cea8be5296167174283
Sarad <saradrana12@gmail.com>
2022-03-02 23:56:35 +0530

    Added some more files

---
+++ Strings/Is Subsequence.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+         int i=0,j=0;
+        if(s.size()==0)
+        {
+            return true;
+        }
+        while(i<s.size() && j<t.size())
+        {
+            if(s[i]==t[j])
+            {
+                i++;
+            }
+            j++;
+        }
+        return i==s.size();
+    }
+};
